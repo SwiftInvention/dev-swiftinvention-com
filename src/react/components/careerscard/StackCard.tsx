cfde1b18d0c4c96983502d3cd7b710908fbe1ce8
saiapowainur <saiapow.ainur@gmail.com>
2025-02-20 14:11:44 +0700

    fix: a11y

--- src/react/components/careerscard/StackCard.tsx
+++ src/react/components/careerscard/StackCard.tsx
@@ -8,13 +8,17 @@ export interface StackCardProps {
 
 export const StackCard: React.FC<StackCardProps> = ({ text, image, link }) => {
   return (
-    <a href={link} target="_blank">
-      <li className="flex h-24 w-full flex-row items-center justify-start gap-6 bg-white/5 p-4 shadow-card hover:shadow-cardHovered md:p-6">
+    <li className="h-24">
+      <a
+        href={link}
+        target="_blank"
+        className="flex h-full w-full flex-row items-center justify-start gap-6 bg-white/5 p-4 shadow-card hover:shadow-cardHovered md:p-6"
+      >
         <img className="h-14 w-14 object-contain" src={image} alt="service" />
         <div className="text-left font-sans text-lg font-normal text-white">
           {text}
         </div>
-      </li>
-    </a>
+      </a>
+    </li>
   );
 };
