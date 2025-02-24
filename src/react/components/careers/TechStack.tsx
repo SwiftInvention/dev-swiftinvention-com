6fee87f4f1e0ffb95598dbde5f01a19c962f88ea
saiapowainur <saiapow.ainur@gmail.com>
2025-02-24 15:49:59 +0700

    feat: add new technologies to all tech stack block

--- src/react/components/careers/TechStack.tsx
+++ src/react/components/careers/TechStack.tsx
@@ -19,7 +19,6 @@ import viteImage from '~/assets/images/stack/vite.svg';
 import tailwindImage from '~/assets/images/stack/tailwind.svg';
 import i18nextImage from '~/assets/images/stack/i18next.svg';
 import solidJsImage from '~/assets/images/stack/solidjs.svg';
-import wcImage from '~/assets/images/stack/webcomponents.svg';
 import rnImage from '~/assets/images/stack/reactnative.svg';
 import swiftImage from '~/assets/images/stack/swift.svg';
 import swiftUiImage from '~/assets/images/stack/swiftui.svg';
@@ -29,7 +28,33 @@ import ajcImage from '~/assets/images/stack/ajc.svg';
 import jotaiImage from '~/assets/images/stack/jotai.png';
 import mysqlImage from '~/assets/images/stack/mysql.png';
 import pgsqlImage from '~/assets/images/stack/pgsql.png';
-import grpcImage from '~/assets/images/stack/grpc.png';
+
+import knexjsImage from '~/assets/images/stack/knexjs.png';
+import ansibleImage from '~/assets/images/stack/ansible.svg';
+import astroImage from '~/assets/images/stack/astro.svg';
+import axeImage from '~/assets/images/stack/axe.png';
+import graphQlImage from '~/assets/images/stack/graphQl.svg';
+import websocketsImage from '~/assets/images/stack/websockets.svg';
+import playwrightImage from '~/assets/images/stack/playwright.png';
+import duckDBImage from '~/assets/images/stack/duckDB.svg';
+import fastAPIImage from '~/assets/images/stack/fastAPI.png';
+import pythonImage from '~/assets/images/stack/python.webp';
+import pyTorchImage from '~/assets/images/stack/pytorch.svg';
+import tRPCImage from '~/assets/images/stack/trpc.svg';
+import typeORMImage from '~/assets/images/stack/typeorm.png';
+import redisImage from '~/assets/images/stack/redis.svg';
+import elasticSearchImage from '~/assets/images/stack/elasticSearch.png';
+import cassandraImage from '~/assets/images/stack/cassandra.png';
+import clickhouseImage from '~/assets/images/stack/clickhouse.svg';
+import sqliteImage from '~/assets/images/stack/sqlite.png';
+import autogenImage from '~/assets/images/stack/autogen.png';
+import mlFlowImage from '~/assets/images/stack/mlflow.svg';
+import genAIImage from '~/assets/images/stack/genai.png';
+import sparkImage from '~/assets/images/stack/spark.svg';
+import icebergImage from '~/assets/images/stack/iceberg.png';
+import flinkImage from '~/assets/images/stack/flink.png';
+import nifiImage from '~/assets/images/stack/nifi.png';
+import kafkaImage from '~/assets/images/stack/kafka.png';
 import { StackCard } from '../careerscard/StackCard';
 
 const techStackList: {
@@ -39,59 +64,16 @@ const techStackList: {
   disabled?: boolean;
 }[] = [
   {
-    text: 'Scala',
-    image: scalaImage.src,
-    link: 'https://www.scala-lang.org/',
-  },
-  {
-    text: 'Java',
-    image: javaImage.src,
-    link: 'https://www.oracle.com/java/',
-  },
-  {
-    text: 'Rust',
-    image: rustImage.src,
-    link: 'https://www.rust-lang.org/',
-  },
-  {
-    text: 'TypeScript',
-    image: tsImage.src,
-    link: 'https://www.typescriptlang.org/',
-  },
-  {
-    text: 'JavaScript',
-    image: jsImage.src,
-    link: 'https://www.ecma-international.org/publications-and-standards/standards/ecma-262/',
-  },
-  {
-    text: 'ZIO',
-    image: zioImage.src,
-    link: 'https://zio.dev/',
-  },
-  {
     text: 'Akka',
     image: akkaImage.src,
     link: 'https://akka.io/',
   },
+  { text: 'Astro', image: astroImage.src, link: '' },
+  { text: 'Ansible', image: ansibleImage.src, link: '' },
   {
-    text: 'Grafana',
-    image: grafanaImage.src,
-    link: 'https://grafana.com/',
-  },
-  {
-    text: 'MySQL',
-    image: mysqlImage.src,
-    link: 'https://www.mysql.com/',
-  },
-  {
-    text: 'PostgreSQL',
-    image: pgsqlImage.src,
-    link: 'https://www.postgresql.org/',
-  },
-  {
-    text: 'gRPC',
-    image: grpcImage.src,
-    link: 'https://grpc.io/',
+    text: 'AWS',
+    image: awsImage.src,
+    link: 'https://aws.amazon.com/',
   },
   {
     text: 'Gatling',
@@ -99,24 +81,9 @@ const techStackList: {
     link: 'https://gatling.io/',
   },
   {
-    text: 'ZAP',
-    image: zapImage.src,
-    link: 'https://www.zaproxy.org/',
-  },
-  {
-    text: 'AWS',
-    image: awsImage.src,
-    link: 'https://aws.amazon.com/',
-  },
-  {
-    text: 'GCP',
-    image: gcpImage.src,
-    link: 'https://cloud.google.com/',
-  },
-  {
-    text: 'Kubernetes',
-    image: kubernetesImage.src,
-    link: 'https://kubernetes.io/',
+    text: 'Grafana',
+    image: grafanaImage.src,
+    link: 'https://grafana.com/',
   },
   {
     text: 'Istio',
@@ -124,14 +91,80 @@ const techStackList: {
     link: 'https://istio.io/',
   },
   {
+    text: 'Java',
+    image: javaImage.src,
+    link: 'https://www.oracle.com/java/',
+  },
+  {
+    text: 'JavaScript',
+    image: jsImage.src,
+    link: 'https://www.ecma-international.org/publications-and-standards/standards/ecma-262/',
+  },
+  {
+    text: 'Kubernetes',
+    image: kubernetesImage.src,
+    link: 'https://kubernetes.io/',
+  },
+  { text: 'Knex.js', image: knexjsImage.src, link: '' },
+  {
+    text: 'PostgreSQL',
+    image: pgsqlImage.src,
+    link: 'https://www.postgresql.org/',
+  },
+  {
     text: 'Prometheus',
     image: prometheusImage.src,
     link: 'https://prometheus.io/',
+  },
+  { text: 'Python', image: pythonImage.src, link: '' },
+  { text: 'PyTorch', image: pyTorchImage.src, link: '' },
+  { text: 'React', image: rnImage.src, link: '' },
+  {
+    text: 'React Native',
+    image: rnImage.src,
+    link: 'https://reactnative.dev/',
+  },
+  {
+    text: 'Rust',
+    image: rustImage.src,
+    link: 'https://www.rust-lang.org/',
+  },
+  {
+    text: 'Scala',
+    image: scalaImage.src,
+    link: 'https://www.scala-lang.org/',
   },
   {
     text: 'Terraform',
     image: terraformImage.src,
     link: 'https://www.terraform.io/',
+  },
+  { text: 'tRPC', image: tRPCImage.src, link: '' },
+  { text: 'TypeORM', image: typeORMImage.src, link: '' },
+  {
+    text: 'TypeScript',
+    image: tsImage.src,
+    link: 'https://www.typescriptlang.org/',
+  },
+  {
+    text: 'ZAP',
+    image: zapImage.src,
+    link: 'https://www.zaproxy.org/',
+  },
+  {
+    text: 'ZIO',
+    image: zioImage.src,
+    link: 'https://zio.dev/',
+  },
+  {
+    text: 'MySQL',
+    image: mysqlImage.src,
+    link: 'https://www.mysql.com/',
+  },
+  {
+    text: 'GCP',
+    image: gcpImage.src,
+    link: 'https://cloud.google.com/',
   },
   {
     text: 'Vite',
@@ -154,19 +187,9 @@ const techStackList: {
     link: 'https://www.solidjs.com/',
   },
   {
-    text: 'WebComponents',
-    image: wcImage.src,
-    link: 'https://www.webcomponents.org/',
-  },
-  {
     text: 'Jotai',
     image: jotaiImage.src,
     link: 'https://jotai.org/',
-  },
-  {
-    text: 'React Native',
-    image: rnImage.src,
-    link: 'https://reactnative.dev/',
   },
   {
     text: 'Swift',
@@ -188,6 +211,25 @@ const techStackList: {
     image: ajcImage.src,
     link: 'https://developer.android.com/jetpack/compose',
   },
+  { text: 'Axe', image: axeImage.src, link: '' },
+  { text: 'GraphQl', image: graphQlImage.src, link: '' },
+  { text: 'Websockets', image: websocketsImage.src, link: '' },
+  { text: 'Playwright', image: playwrightImage.src, link: '' },
+  { text: 'DuckDB', image: duckDBImage.src, link: '' },
+  { text: 'Redis', image: redisImage.src, link: '' },
+  { text: 'Elastic Search', image: elasticSearchImage.src, link: '' },
+  { text: 'Cassandra', image: cassandraImage.src, link: '' },
+  { text: 'Clickhouse', image: clickhouseImage.src, link: '' },
+  { text: 'SQLite', image: sqliteImage.src, link: '' },
+  { text: 'Autogen', image: autogenImage.src, link: '' },
+  { text: 'MLFlow', image: mlFlowImage.src, link: '' },
+  { text: 'FastAPI', image: fastAPIImage.src, link: '' },
+  { text: 'GenAI', image: genAIImage.src, link: '' },
+  { text: 'Spark', image: sparkImage.src, link: '' },
+  { text: 'Iceberg', image: icebergImage.src, link: '' },
+  { text: 'Flink', image: flinkImage.src, link: '' },
+  { text: 'Nifi', image: nifiImage.src, link: '' },
+  { text: 'Kafka', image: kafkaImage.src, link: '' },
 ];
 
 export const TechStack: React.FC = () => {
